149ba083a6ff76ad2e41a52ea7ed0afbd97da40d
Apple <apple@Apple-MacBook.local>
2023-11-03 01:33:35 +0100

    0.01

--- fucking-test-next-line-mechanix.c
+++ fucking-test-next-line-mechanix.c
@@ -1,24 +1,70 @@
+// #include <aio.h>
+// #include <fcntl.h>  // For file control options like O_RDONLY
+// #include <unistd.h> // For system calls like open()
+// #include <stdio.h>
 
-#include <aio.h>
-#include <fcntl.h>  // For file control options like O_RDONLY
-#include <unistd.h> // For system calls like open()
+// int main() {
+// 	char **buffer;
+// 	int fd = open("example.txt", O_RDONLY);
+// 	if (fd == -1) {
+// 		perror("Error opening the file");
+// 		return 1;
+// 	}
+// 	ssize_t bytesRead = read(fd, buffer[1], 123);
+// 	buffer[1][bytesRead] = '\0';
+// 	printf("Read from the file: %s", buffer[1]);
+// 	close(fd);
+// 	return 0;
+// }
+
+
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
-
+#define MAX_STRINGS 10 // Maximum number of strings to read
+#define MAX_STRING_LENGTH 1024 // Maximum string length
 
 int main() {
-	char buffer[6969];
-	int fd = open("example.txt", O_RDONLY);
-	if (fd == -1) {
-		perror("Error opening the file");
-		return 1;
-	}
-	ssize_t bytesRead = read(fd, buffer, 12313132123);
-	buffer[bytesRead] = '\0';
-	printf("Read from the file: %s", buffer);
-	close(fd);
-	return 0;
+    char buffer[MAX_STRINGS][MAX_STRING_LENGTH]; // Array to store multiple strings
+    int fd = open("example.txt", O_RDONLY);
+
+    if (fd == -1) {
+        perror("Error opening the file");
+        return 1;
+    }
+
+    int stringCount = 0; // Counter for the number of strings read
+
+    // Read strings until we reach the end of the file or reach the limit
+    while (stringCount < MAX_STRINGS) {
+        ssize_t bytesRead = read(fd, buffer[stringCount], MAX_STRING_LENGTH);
+
+        if (bytesRead == -1) {
+            perror("Error reading from the file");
+            close(fd);
+            return 1;
+        } else if (bytesRead == 0) {
+            // End of file reached
+            break;
+        } else {
+            buffer[stringCount][bytesRead] = '\0'; // Null-terminate the string
+            printf("Read from the file: %s", buffer[stringCount]);
+            stringCount++;
+        }
+    }
+
+    close(fd);
+
+    // Now you have stringCount strings in the buffer array.
+    for (int i = 0; i < stringCount; i++) {
+        printf("Array[%d]: %s\n", i, buffer[i]);
+    }
+
+    return 0;
 }
+
 
 /*
 SOO i REALLy want to implement soemthing liek sizeof()
--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -10,3 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int			s_len;
+	const char	*end;
+
+	s_len = ft_strlen(s);
+	end = s + s_len - 1;
+	if (c == '\0')
+		return ((char *)s + s_len);
+	while (end >= s)
+	{
+		if (*end == (char)c)
+			return ((char *)end);
+		end--;
+	}
+	return (NULL);
+}
